c550553f35b940dcb64f89092f3ba018e6662c5d
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2025-05-10 23:34:59 +0800

    fixed error

--- monopoly/Game/GameConfig.cpp
+++ monopoly/Game/GameConfig.cpp
@@ -7,7 +7,7 @@
 GameConfig GameConfig::instance;
 
 // Singleton accessor
-GameConfig& GameConfig::getInstance()
+GameConfig &GameConfig::getInstance()
 {
 	return instance;
 }
@@ -35,9 +35,9 @@ void GameConfig::loadConfig()
 	fileConfig >> config;
 	fileDialogue >> dialogue;
 
-	// If the mode is not release, read all the value from the specified mode
 	// If the mode is release, ask the user to input the number of players and the player names
-	std::string mode = config["mode"].get<std::string>();
+	// If the mode is not release, read all the value from the specified mode in config.json
+	std::string mode = config["defaultMode"].get<std::string>();
 	if (mode == "RELEASE")
 	{
 		std::cout << dialogue["input_player_num"].get<std::string>() << "\n";
@@ -53,14 +53,14 @@ void GameConfig::loadConfig()
 	else
 	{
 		// Parse the built-in player-related configurations
-		playersNum = config["mode"][mode]["playersNum"].get<int>();
-		playerNames = config["playerNames"].get<std::vector<std::string>>();
+		playersNum = config["modes"][mode]["playersNum"].get<int>();
+		playerNames = config["modes"][mode]["playersName"].get<std::vector<std::string>>();
 	}
 
 	// Parse game mode configurations
-	startMoney = config["mode"][mode]["startMoney"].get<int>();
-	winMoney = config["mode"][mode]["winMoney"].get<int>();
-	passingStartBonus = config["mode"][mode]["passingStartBonus"].get<int>();
+	startMoney = config["modes"][mode]["startMoney"].get<int>();
+	winMoney = config["modes"][mode]["winMoney"].get<int>();
+	passingStartBonus = config["modes"][mode]["passingStartBonus"].get<int>();
 
 	// Parse default player-related configurations
 	playerIcons = config["playerIcons"].get<std::vector<std::string>>();
@@ -73,7 +73,7 @@ void GameConfig::loadConfig()
 	propertyLevelIcons = config["propertyLevelIcons"].get<std::vector<std::string>>();
 
 	// Parse the board tiles
-	for (const auto& tile : config["boardTiles"])
+	for (const auto &tile : config["boardTiles"])
 	{
 		TileConfig tileConfig;
 		tileConfig.id = tile["index"].get<std::size_t>();
@@ -82,10 +82,11 @@ void GameConfig::loadConfig()
 		tileConfig.price = tile["price"].get<int>();
 		tileConfig.toll = tile["toll"].get<int>();
 		boardTiles.push_back(tileConfig);
+		locationMap[tileConfig.id] = tileConfig.name;
 	}
 
 	// Parse the cards
-	for (const auto& card : config["cards"])
+	for (const auto &card : config["cards"])
 	{
 		CardConfig cardConfig;
 		cardConfig.name = card["name"].get<std::string>();
@@ -95,10 +96,13 @@ void GameConfig::loadConfig()
 		cards.push_back(cardConfig);
 	}
 
-	// Parse the location map
-	for (const auto& location : config["locationMap"].items())
+	// Parse eventValueRange
+	for (const auto &event : config["eventValueRange"].items())
 	{
-		locationMap[std::stoi(location.key())] = location.value().get<std::string>();
+		std::string eventName = event.key();
+		int minValue = event.value()[0].get<int>(),
+			maxValue = event.value()[1].get<int>();
+		eventValueRange[eventName] = std::make_pair(minValue, maxValue);
 	}
 }
 
@@ -109,16 +113,16 @@ GameMode GameConfig::getMode() const { return mode; }
 void GameConfig::setPlayersNum(int num) { playersNum = num; }
 int GameConfig::getPlayersNum() const { return playersNum; }
 
-void GameConfig::setPlayersName(const std::vector<std::string>& names) { playerNames = names; }
+void GameConfig::setPlayersName(const std::vector<std::string> &names) { playerNames = names; }
 std::vector<std::string> GameConfig::getPlayerNames() const { return playerNames; }
 
-void GameConfig::setPlayerIcons(const std::vector<std::string>& icons) { playerIcons = icons; }
+void GameConfig::setPlayerIcons(const std::vector<std::string> &icons) { playerIcons = icons; }
 std::vector<std::string> GameConfig::getPlayerIcons() const { return playerIcons; }
 
-void GameConfig::setPlayerColors(const std::vector<std::string>& colors) { playerColors = colors; }
+void GameConfig::setPlayerColors(const std::vector<std::string> &colors) { playerColors = colors; }
 std::vector<std::string> GameConfig::getPlayerColors() const { return playerColors; }
 
-void GameConfig::setPropertyLevelIcons(const std::vector<std::string>& icons) { propertyLevelIcons = icons; }
+void GameConfig::setPropertyLevelIcons(const std::vector<std::string> &icons) { propertyLevelIcons = icons; }
 std::vector<std::string> GameConfig::getPropertyLevelIcons() const { return propertyLevelIcons; }
 
 std::map<int, std::string> GameConfig::getLocationMap() const { return locationMap; }
@@ -132,16 +136,16 @@ int GameConfig::getWinMoney() const { return winMoney; }
 void GameConfig::setPassingStartBonus(int amount) { passingStartBonus = amount; }
 int GameConfig::getPassingStartBonus() const { return passingStartBonus; }
 
-void GameConfig::setBoardTiles(const std::vector<TileConfig>& tiles) { boardTiles = tiles; }
+void GameConfig::setBoardTiles(const std::vector<TileConfig> &tiles) { boardTiles = tiles; }
 std::vector<TileConfig> GameConfig::getBoardTiles() const { return boardTiles; }
 
-void GameConfig::setCards(const std::vector<CardConfig>& cards)
+void GameConfig::setCards(const std::vector<CardConfig> &cards)
 {
 	// cards = cards;
 }
 std::vector<CardConfig> GameConfig::getCards() const { return cards; }
 
-void GameConfig::setEventValueRange(const std::map<std::string, std::pair<int, int>>& range) { eventValueRange = range; }
+void GameConfig::setEventValueRange(const std::map<std::string, std::pair<int, int>> &range) { eventValueRange = range; }
 std::map<std::string, std::pair<int, int>> GameConfig::getEventValueRange() const { return eventValueRange; }
 
 void GameConfig::setAnimation(bool status) { animation = status; }
--- monopoly/main.cpp
+++ monopoly/main.cpp
@@ -4,9 +4,11 @@
 
 int main()
 {
-	WelcomingScreen welcomeScreen;
-	welcomeScreen.displayWelcomeScreen();
-	welcomeScreen.displayStartGame();
+	// Display the welcoming screen for the first 3 seconds
+	// WARNING: This part is commented out in development mode to increase efficiency
+	// WelcomingScreen welcomeScreen;
+	// welcomeScreen.displayWelcomeScreen();
+	// welcomeScreen.displayStartGame();
 
 	// Load game configuration
 	GameConfig::getInstance().loadConfig();
