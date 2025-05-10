955b1a6f75576e4f9ba52a812d2fcb7756be8b94
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2025-05-10 17:35:48 +0800

    updated gameConfig stucture

--- monopoly/GameConfig.cpp
+++ monopoly/GameConfig.cpp
@@ -1,34 +1,154 @@
 #include "GameConfig.h"
+
+#include <nlohmann/json.hpp>
 #include <fstream>
 #include <iostream>
-#include "json.hpp"
 
-using json = nlohmann::json;
+GameConfig GameConfig::instance;
 
-GameConfig& GameConfig::getInstance() {
-	static GameConfig instance;
+// Singleton accessor
+GameConfig &GameConfig::getInstance()
+{
 	return instance;
 }
 
-bool GameConfig::loadConfig(const std::string& filename) {
-	std::ifstream file(filename);
-	if (!file.is_open()) {
-		std::cerr << "Cannot open config file: " << filename << "\n";
-		return false;
+// Load configuration from JSON file
+void GameConfig::loadConfig()
+{
+	std::string configPath = "monopoly/json/config.json";
+	std::string dialoguePath = "monopoly/json/dialogue.json";
+	std::ifstream fileConfig(configPath);
+	std::ifstream fileDialogue(dialoguePath);
+
+	if (!fileDialogue.is_open())
+	{
+		std::cerr << "Cannot open dialogue file: " << dialoguePath << "\n";
+		return;
+	}
+	if (!fileConfig.is_open())
+	{
+		std::cerr << "Cannot open config file: " << configPath << "\n";
+		return;
 	}
 
-	json j;
-	file >> j;
+	nlohmann::json config, dialogue;
+	fileConfig >> config;
+	fileDialogue >> dialogue;
 
-	startMoney = j.value("startMoney", 5000);
-	winMoney = j.value("winMoney", 20000);
-	passingStartBonus = j.value("passingStartBonus", 2000);
-	mapSize = j.value("mapSize", 32);
+	// If the mode is not release, read all the value from the specified mode
+	// If the mode is release, ask the user to input the number of players and the player names
+	std::string mode = config["mode"].get<std::string>();
+	if (mode == "RELEASE")
+	{
+		std::cout << dialogue["input_player_num"].get<std::string>() << "\n";
+		std::cin >> playersNum;
 
-	return true;
+		playerNames.resize(playersNum);
+		for (int i = 0; i < playersNum; ++i)
+		{
+			std::cout << dialogue["input_player_name"].get<std::string>() << " " << i + 1 << ": ";
+			std::getline(std::cin, playerNames[i]);
+		}
+	}
+	else
+	{
+		// Parse the built-in player-related configurations
+		playersNum = config["mode"][mode]["playersNum"].get<int>();
+		playerNames = config["playerNames"].get<std::vector<std::string>>();
+	}
+
+	// Parse game mode configurations
+	startMoney = config["mode"][mode]["startMoney"].get<int>();
+	winMoney = config["mode"][mode]["winMoney"].get<int>();
+	passingStartBonus = config["mode"][mode]["passingStartBonus"].get<int>();
+
+	// Parse default player-related configurations
+	playerIcons = config["playerIcons"].get<std::vector<std::string>>();
+	playerColors = config["playerColors"].get<std::vector<std::string>>();
+
+	mapSize = config["mapSize"].get<int>();
+	tileWidth = config["tileWidth"].get<size_t>();
+	animation = config["animation"].get<bool>();
+	animationTime = config["animationTime"].get<long long>();
+	propertyLevelIcons = config["propertyLevelIcons"].get<std::vector<std::string>>();
+
+	// Parse the board tiles
+	for (const auto &tile : config["boardTiles"])
+	{
+		TileConfig tileConfig;
+		tileConfig.id = tile["index"].get<std::size_t>();
+		tileConfig.type = tile["type"].get<std::string>();
+		tileConfig.name = tile["name"].get<std::string>();
+		tileConfig.price = tile["price"].get<int>();
+		tileConfig.toll = tile["toll"].get<int>();
+		boardTiles.push_back(tileConfig);
+	}
+
+	// Parse the cards
+	for (const auto &card : config["cards"])
+	{
+		CardConfig cardConfig;
+		cardConfig.name = card["name"].get<std::string>();
+		cardConfig.icon = card["icon"].get<std::string>();
+		cardConfig.effect = card["effect"].get<std::string>();
+		cardConfig.price = card["price"].get<int>();
+		cards.push_back(cardConfig);
+	}
+
+	// Parse the location map
+	for (const auto &location : config["locationMap"].items())
+	{
+		locationMap[std::stoi(location.key())] = location.value().get<std::string>();
+	}
 }
 
+// Setters and getters for GameConfig properties
+void GameConfig::setMode(GameMode newMode) { mode = newMode; }
+GameMode GameConfig::getMode() const { return mode; }
+
+void GameConfig::setPlayersNum(int num) { playersNum = num; }
+int GameConfig::getPlayersNum() const { return playersNum; }
+
+void GameConfig::setPlayersName(const std::vector<std::string> &names) { playerNames = names; }
+std::vector<std::string> GameConfig::getPlayerNames() const { return playerNames; }
+
+void GameConfig::setPlayerIcons(const std::vector<std::string> &icons) { playerIcons = icons; }
+std::vector<std::string> GameConfig::getPlayerIcons() const { return playerIcons; }
+
+void GameConfig::setPlayerColors(const std::vector<std::string> &colors) { playerColors = colors; }
+std::vector<std::string> GameConfig::getPlayerColors() const { return playerColors; }
+
+void GameConfig::setPropertyLevelIcons(const std::vector<std::string> &icons) { propertyLevelIcons = icons; }
+std::vector<std::string> GameConfig::getPropertyLevelIcons() const { return propertyLevelIcons; }
+
+std::map<int, std::string> GameConfig::getLocationMap() const { return locationMap; }
+
+void GameConfig::setStartMoney(int amount) { startMoney = amount; }
 int GameConfig::getStartMoney() const { return startMoney; }
+
+void GameConfig::setWinMoney(int amount) { winMoney = amount; }
 int GameConfig::getWinMoney() const { return winMoney; }
+
+void GameConfig::setPassingStartBonus(int amount) { passingStartBonus = amount; }
 int GameConfig::getPassingStartBonus() const { return passingStartBonus; }
+
+void GameConfig::setBoardTiles(const std::vector<TileConfig> &tiles) { boardTiles = tiles; }
+std::vector<TileConfig> GameConfig::getBoardTiles() const { return boardTiles; }
+
+void GameConfig::setCards(const std::vector<CardConfig> &cards)
+{
+	// cards = cards;
+}
+std::vector<CardConfig> GameConfig::getCards() const { return cards; }
+
+void GameConfig::setEventValueRange(const std::map<std::string, std::pair<int, int>> &range) { eventValueRange = range; }
+std::map<std::string, std::pair<int, int>> GameConfig::getEventValueRange() const { return eventValueRange; }
+
+void GameConfig::setAnimation(bool status) { animation = status; }
+bool GameConfig::getAnimation() const { return animation; }
+
 int GameConfig::getMapSize() const { return mapSize; }
+
+int GameConfig::getTileWidth() const { return tileWidth; }
+
+long long GameConfig::getAnimationTime() const { return animationTime; }
--- monopoly/GameConfig.h
+++ monopoly/GameConfig.h
@@ -1,26 +1,124 @@
 #pragma once
+
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
 
-class GameConfig {
+enum class GameMode
+{
+	DEBUG,
+	DUEL,
+	RELEASE,
+	CUSTOM
+};
+
+struct TileConfig
+{
+	std::size_t id;
+	std::string type;
+	std::string name;
+
+	int price;
+	int toll;
+};
+
+struct CardConfig
+{
+	std::string name;
+	std::string icon;
+	std::string effect;
+
+	int price;
+};
+
+class GameConfig
+{
+private:
+	static GameConfig instance;
+	GameMode mode = GameMode::DEBUG;
+
+	int playersNum = 2;
+	int startMoney = 0;
+	int winMoney = 0;
+	int passingStartBonus = 0;
+	int mapSize = 8;
+	size_t tileWidth = 14;
+
+	bool animation = true;
+	long long animationTime = 0;
+
+	std::vector<std::string> playerNames;
+	std::vector<std::string> playerIcons;
+	std::vector<std::string> playerColors;
+
+	std::vector<TileConfig> boardTiles;
+	std::vector<CardConfig> cards;
+	std::vector<std::string> propertyLevelIcons;
+	std::map<int, std::string> locationMap;
+	std::map<std::string, std::pair<int, int>> eventValueRange;
+
+	GameConfig() = default;
+
 public:
-	static GameConfig& getInstance();
+	static GameConfig &getInstance();
 
-	bool loadConfig(const std::string& filename = "config.json");
+	// Set and get game mode
+	void setMode(GameMode newMode);
+	GameMode getMode() const;
 
+	// Set and get number of players
+	void setPlayersNum(int num);
+	int getPlayersNum() const;
+
+	// Set and get player names
+	void setPlayersName(const std::vector<std::string> &names);
+	std::vector<std::string> getPlayerNames() const;
+
+	// Set and get player icons
+	void setPlayerIcons(const std::vector<std::string> &icons);
+	std::vector<std::string> getPlayerIcons() const;
+
+	// Set and get player colors
+	void setPlayerColors(const std::vector<std::string> &colors);
+	std::vector<std::string> getPlayerColors() const;
+
+	// Set and get property level icons
+	void setPropertyLevelIcons(const std::vector<std::string> &icons);
+	std::vector<std::string> getPropertyLevelIcons() const;
+
+	std::map<int, std::string> getLocationMap() const;
+
+	// Set and get money information
+	void setStartMoney(int amount);
 	int getStartMoney() const;
+
+	void setWinMoney(int amount);
 	int getWinMoney() const;
+
+	void setPassingStartBonus(int amount);
 	int getPassingStartBonus() const;
+
+	// Set and get board tile information
+	void setBoardTiles(const std::vector<TileConfig> &tiles);
+	std::vector<TileConfig> getBoardTiles() const;
+
+	// Set and get cards information
+	void setCards(const std::vector<CardConfig> &cards);
+	std::vector<CardConfig> getCards() const;
+
+	// Set and get event value range
+	void setEventValueRange(const std::map<std::string, std::pair<int, int>> &range);
+	std::map<std::string, std::pair<int, int>> getEventValueRange() const;
+
+	// Set and get animation status
+	void setAnimation(bool status);
+	bool getAnimation() const;
+
 	int getMapSize() const;
 
-private:
-	GameConfig() = default;
-	GameConfig(const GameConfig&) = delete;
-	GameConfig& operator=(const GameConfig&) = delete;
+	int getTileWidth() const;
 
-	int startMoney = 5000;
-	int winMoney = 20000;
-	int passingStartBonus = 2000;
-	int mapSize = 32;
+	long long getAnimationTime() const;
+
+	void loadConfig();
 };
